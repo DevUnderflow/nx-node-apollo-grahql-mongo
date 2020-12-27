517fffa0cdee1d5d29731f3c2765c08408641878
Smith Gajjar <smith.gajjar@gmail.com>
2020-12-27 19:16:56 +0530

    wip: server entry point

---
+++ apps/api/src/main.ts
@@ -0,0 +1,10 @@
+/**
+ * For Typegraphql & Typegoose Exmaples
+ * https://github.com/MichalLytek/type-graphql/tree/master/examples
+ *
+ * For Redis GUI use
+ * redis-commander - https://www.npmjs.com/package/redis-commander
+ */
+
+import startServer from './app';
+new startServer();
